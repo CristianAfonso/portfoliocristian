8b15cc119d5f40e21f3cb43424ff609751f64998
Cristian Afonso Nieto <can97clase@gmail.com>
2023-10-03 19:42:59 +0100

    Elimino cosas base de next

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,25 +25,6 @@ export default function Home() {
               web utilizando la API de GitHub
             </p>
           </Link>
-
-          <a href="https://nextjs.org/learn" >
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples">
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
